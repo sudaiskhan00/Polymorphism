28748ec4e93a317a8bdd15969e6cf446f26fd1b1
sudaiskhan00 <sudaismurad3@gmail.com>
2025-03-16 21:35:35 +0500

    Add files via upload

---
+++ withOutPrinciple/SportsCar.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package withOutPrinciple;
+
+/**
+ *
+ * @author lenovo
+ */
+class SportsCar {
+    void start() {
+        System.out.println("The sports car starts with a loud vroom");
+    }
+}
+
+// ElectricCar class
+class ElectricCar {
+    void start() {
+        System.out.println("The electric car starts silent");
+    }
+}
+
